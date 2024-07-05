229cbb2470f4d03a35c6340942f26304cd131222
Aliaksandr Naumavets <naum1979alex@gmail.com>
2024-07-06 02:02:50 +0300

    chore: configure prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'react-compiler'],
+  plugins: ['react-refresh', 'react-compiler', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
@@ -18,4 +20,4 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
   },
-}
+};
