2573565fb45811bb952a2b12a0e2349085a7ebf0
Aliaksandr Naumavets <naum1979alex@gmail.com>
2024-07-22 17:06:08 +0300

    refactor: move routes to separate file

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { routes } from './routes/router.tsx';
 
-import { AnimalDetails } from './components/AnimalDetails/AnimalDetails.tsx';
-import { animalLoader } from './routes/animalLoader.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: '',
-    element: <App />,
-
-    children: [
-      {
-        path: 'details/:id',
-        element: <AnimalDetails />,
-        loader: animalLoader,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
