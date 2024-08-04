6ddce9733845d32310ea2d2b646840dc4c337f8f
Aliaksandr Naumavets <naum1979alex@gmail.com>
2024-08-04 22:01:46 +0300

    feat: substitute vite with next js

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { ErrorBoundary } from './components/Error-boundary';
 import { ThemeProvider } from './components/ThemeProvider/ThemeProvider';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes/router';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <ThemeProvider>
-      <ErrorBoundary>
-        <RouterProvider router={router} />
-      </ErrorBoundary>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <RouterProvider router={router} />
+        </ErrorBoundary>
+      </ThemeProvider>
+    </Provider>
   );
 }
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './store/store.tsx';
-import App from './App.tsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
