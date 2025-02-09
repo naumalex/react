f6bc48fbd584e09010d591a265f4cff5d0b21627
Aliaksandr Naumavets <naum1979alex@gmail.com>
2025-02-09 14:59:11 +0300

    chore: setup gallea action

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       exclude: [...configDefaults.exclude, 'build/*', '*.mjs', '*.cjs'],
       provider: 'istanbul',
-      reporter: ['text', 'html', 'clover', 'json', 'cobertura'],
+      reporter: ['text', 'html', 'clover', 'json', 'cobertura', 'junit'],
     },
   },
 });
